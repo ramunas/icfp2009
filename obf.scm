61447214020cd3e2f028e074432664564750bd4d
Ramūnas Gutkovas <ramunasg@gmail.com>
2009-06-27 12:18:15 +0300

    instructions

--- obf.scm
+++ obf.scm
@@ -8,11 +8,11 @@
 
 
   (define (read-d64le port)
-    (bytevector-ieee-double-ref 
+    (bytevector-ieee-double-ref
       (get-bytevector-n port 8) 0 (endianness little)))
 
 
-  (define (read-i43le port)
+  (define (read-i32le port)
     (bytevector-s32-ref
       (get-bytevector-n port 4) 0 (endianness little)))
 
@@ -22,8 +22,8 @@
 
   (define (read-frame evn? port)
     (if evn?
-      (cons (read-d64le port) (read-i43le port))
-      (xchng (cons (read-i43le port) (read-d64le port)))))
+      (cons (read-d64le port) (read-i32le port))
+      (xchng (cons (read-i32le port) (read-d64le port)))))
 
 
   (define (dump-obf file)
@@ -39,7 +39,8 @@
             (newline)
             (loop (not evn) (+ ins 1)))))
       (close-port port)))
-    
+
+
 
   (define d-type-instr '((1 . add )
                          (2 . sub )
@@ -55,7 +56,7 @@
                          (4 . inpt)))
 
   (define imm-op-code  '((0 . <)
-                         (1 . =<)
+                         (1 . <=)
                          (2 . =)
                          (3 . >=)
                          (4 . >)))
@@ -77,18 +78,103 @@
 
 
 
-  (define-record-type orbit-vm (fields 
+
+
+
+  (define-record-type orbit-vm (fields
                                  mem
                                  inp
                                  out
-                                 status
+                                 (mutable status)
                                  pc))
 
-  ;(define (mem vm r)
-    ;(hashtable-ref (orbit-vm-mem vm) r 0.0))
+  (define make-mem make-eq-hashtable)
+  (define (mem-set! mem addr v) (hashtable-set! mem addr v))
+  (define (mem-get  mem addr) (hashtable-ref mem addr))
 
-  ;(define (rd! vm v)
-    ;(hashtable-set! (orbit-vm-mem vm) retrun-address v))
+  (define (make-vm)
+    (make-orbit-vm
+      (make-mem) ; mem
+      (make-mem) ; input
+      (make-mem) ; output
+      #f         ; status
+      0          ; program counter
+      ))
+
+
+  (define (alist->instr-set a) a)
+  (define (instr-set-get set idx) (cdr (assq set idx)))
+
+  (define (make-instr name code) (cons name code))
+  (define (instr-name instr) (car instr))
+  (define (instr-code instr) (cdr instr))
+
+
+  ;; instruction operations
+  (define (mem vm addr)    (mem-get (orbit-vm-mem vm) addr))
+  (define (mem! vm addr v) (mem-set! (orbit-vm-mem vm) addr v))
+  (define (inp vm addr)    (mem-get (orbit-vm-inp vm) addr))
+  (define (out! vm addr v) (mem-set! (orbit-vm-out vm) addr v))
+  (define (status vm) (orbit-vm-status vm))
+  (define (status! vm x) (orbit-vm-status-set! vm x))
+  (define (op-instr op) (lambda (vm rd r1 r2)
+                          (mem! vm rd (op (mem vm r1)
+                                          (mem vm r2)))))
+
+  (define eps 1.0e-20)
+  (define (=0.0 x) (< (abs x) eps))
+  (define (div-op a b)
+    (if (=0.0 b)
+      0.0
+      (/ a b)))
+
+  (define d-type-instr-set (alist->instr-set 
+                             `((1 . ,(make-instr 'add (op-instr +)))
+                               (2 . ,(make-instr 'sub (op-instr -)))
+                               (3 . ,(make-instr 'mul (op-instr *)))
+                               (4 . ,(make-instr 'div (op-instr div-op)))
+                               (5 . ,(make-instr 'out (lambda (vm rd r1 r2)
+                                                        (out! vm r1 
+                                                              (mem vm r2)))))
+                               (6 . ,(make-instr 'phi (lambda (vm rd r1 r2) 
+                                                        (if (status vm)
+                                                          (mem! vm rd
+                                                                (mem vm r1))
+                                                          (mem! vm rd
+                                                                (mem vm r2))))))
+                               )))
+
+  (define s-type-instr-set (alist->instr-set
+                             `((0 . ,(make-instr 'noop (lambda (vm rd imm r1)
+                                                         #f)))
+                               (1 . ,(make-instr 'cmpz (lambda (vm rd imm r1)
+                                                         (status! 
+                                                           vm
+                                                           (imm (mem vm r1))))))
+                               (2 . ,(make-instr 'sqrt (lambda (vm rd imm r1)
+                                                         (mem!
+                                                           vm rd
+                                                           (sqrt (mem r1))))))
+                               (3 . ,(make-instr 'copy (lambda (vm rd imm r1)
+                                                         (mem! vm rd
+                                                               (mem r1)))))
+                               (4 . ,(make-instr 'inpt (lambda (vm rd imm r1)
+                                                         (mem! vm rd
+                                                               (inp vm r1)))))
+                               )))
+
+
+  (define (op0.0 op) (lambda (x) (op x 0.0)))
+
+  (define cmpz-imm-ops (alist->instr-set
+                         `((0 . ,(make-instr '<0.0  (op0.0 <)))
+                           (1 . ,(make-instr '<=0.0 (op0.0 <=)))
+                           (2 . ,(make-instr '=0.0  =0.0))
+                           (3 . ,(make-instr '>=0.0 (op0.0 >=)))
+                           (4 . ,(make-instr '>0.0  (op0.0 >))))))
+
+  (define (step vm code)
+    '())
 
   ;(define (load-obj port)
     ;(let loop ((evn #t)
